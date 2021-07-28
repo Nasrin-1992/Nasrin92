2993c65ce57ab5e54d015a7f5950e163c0637efa
Nasrin-1992 <shamima92@yahoo.com>
2021-07-28 15:14:24 -0400

    hello

--- TestNGProject200926M/src/test/java/com/bit/test/SmokeTest.java
+++ TestNGProject200926M/src/test/java/com/bit/test/SmokeTest.java
@@ -42,38 +42,7 @@ public class SmokeTest extends BastTest {
 	
 	
 	}
-    //how many expection we have---2 expection we have
-	//Ans 1)Checked (all to them) 2) UnChecked
-    //What is exception?
-	//exception is a error its can handle a programmer also it is a  run Time exception
-	//How to run only failed test Casses
-	//befferent between heart assert and soft assert?
-	//what is YAML file?
-	//What is Factory?
-	//Fectory is a kinds of data provider
-	//Factories allow you to create tests dynamically. For example, imagine you want to create a test method that will access a
-	//page on a Web site several times, and you want to invoke it with different values:
-	//The @Test annotation can be put on a class instead of a test method:
-
-	//why testNh is famous from JUnit?
-	/*
-	 * Ans:-TestNG is a testing framework inspired from JUnit and NUnit but introducing some new functionalities 
-	 * that make it more powerful and easier to use, such as:
-
-Annotations.
-Run your tests in arbitrarily big thread pools with various policies available (all methods in their own thread, one thread per test class, etc...).
-Test that your code is multithread safe.
-Flexible test configuration.
-Support for data-driven testing (with @DataProvider).
-Support for parameters.
-Powerful execution model (no more TestSuite).
-Supported by a variety of tools and plug-ins (Eclipse, IDEA, Maven, etc...).
-Embeds BeanShell for further flexibility.
-Default JDK functions for runtime and logging (no dependencies).
-Dependent methods for application server testing.
-TestNG is designed to cover all categories of tests:  unit, functional, end-to-end, integration, etc...
-	 */
-
+   
 
 
 
